b1282ba2d208489e4ca3e9045484f1dadee97186
AbbyNZ <28561684+AbbyNZ@users.noreply.github.com>
2019-02-14 22:17:21 +1000

    Additional changes

--- jobboard.js
+++ jobboard.js
@@ -1,30 +1,27 @@
 // JavaScript Document
-const jobboard = "https://jobs.github.com/positions/21516.json";
+const jobboard = "https://codepen.io/jobs.json";
+let jobsData;
 
-const jobs = [];
+const searchBtn = $('#search-btn').on("click",getJobs);
 
-fetch(jobboard).then(blob => blob.json()).then(data => jobs.push(...data));
+var currentTitle = '', currentCompany = '', currentDescription;
 
-function findMatches(wordToMatch, jobs) {
-	return jobs.filter(place => {
-		const regex = new RegExp(wordToMatch, 'gi');
-		return place.city.match(regex) || place.state.match(regex);
-	});
+function getJobs() {
+	return $.ajax({
+		headers: {
+      Accept: "application/json"
+    },
+    url:jobboard,
+	success: function(jsonJobs){
+		jobsData = JSON.parse(jsonJobs);
+		console.log(jsonJobs);
+	}
+  });
 }
 
-function numberWithCommas(x) {
-	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
-function displayMatches() {
-	const matchArray = findMatches(this.value, jobs);
-	const html = matchArray.map(place => {
-		const regex = new RegExp(this.value, 'gi');
-		const location = place.city.replace(regex, `<span class="h1">${this.vale}</span>`);
-		const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`);
-		return `<li> <span class="name">${location}, ${stateName}</span></li>`;
-	}).join('');
-	suggestions.innerHTML = html;
+function getJobByPosition() {
+	$('.results').text(currentTitle);
+	$('#job').html();
 }
 
 const searchInput = document.querySelector('.search');
